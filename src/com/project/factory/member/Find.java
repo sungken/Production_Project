7a939bc69f62a950e4a71c78a8f4c3de5780c5af
SSong-sh <tjdgp0817@gmail.com>
2024-01-31 15:32:00 +0900

    feat : 아이디(사원번호)/비밀번호 찾기 기능 완성, 레이아웃 수정 필요

--- src/com/project/factory/member/Find.java
+++ src/com/project/factory/member/Find.java
@@ -2,101 +2,107 @@ package com.project.factory.member;
 
 import java.util.Scanner;
 
+import com.project.factory.Main;
+import com.project.factory.resource.Data;
+import com.project.factory.resource.Members;
 import com.project.factory.view.member.FindView;
 
-
 public class Find {
-
-	public static void main(String[] args) {
-
-		Data.load();
-		
-		Scanner scan = new Scanner(System.in);
-		
-		FindView.findMenu();
-		
-		String selNum = scan.nextLine();
-		
-		if(selNum.equals("1")) {
-			findId();
-		}else if(selNum.equals("2")) {
-			findPw();
-		}
-		
-		
-	}//main
-
-
-private static void findId() {
-	Scanner scan = new Scanner(System.in);
-
-	System.out.println("-----------------------------");
 	
-	System.out.print("이름: ");
-	String name = scan.nextLine();
+	//TODO find 메서드 추가
+	//1. 아이디(사원번호) 찾기
+	//2. 비밀먼호 찾기
+    public static void find() {
+        //Data.load();
 
-	System.out.print("전화번호: ");
-	String phoneNum = scan.nextLine();
+        Scanner scan = new Scanner(System.in);
 
-	String memberId = findMemberId(name, phoneNum);
-	
-	System.out.println("-----------------------------");
+        FindView.findMenu();
 
-	if (memberId != null) {
-		System.out.println("아이디(사원번호): " + memberId + " 입니다.");
-	} else {
-		System.out.println("아이디를 찾을 수 없습니다.");
-		System.out.println();
-		System.out.println("계속 진행하시겠습니까?(Y/N)");
-		System.out.print("입력: ");
-		
-		String answer = scan.nextLine();
+        String selNum = scan.nextLine();
 
-		
-	}
-}//findId
-	
+        if (selNum.equals("1")) {
+            findId();
+        } else if (selNum.equals("2")) {
+            findPw();
+        }
 
-public static String findMemberId(String name, String phoneNum) {
-	for (TempMember member : Data.list) {
-		// 이름과 전화번호가 일치하는 경우 해당 멤버의 아이디를 반환
-		if (member.getName().equals(name) && member.getPhoneNum().equals(phoneNum)) {
-			return member.getMemberId();
-		}
-	}
-	// 일치하는 멤버가 없는 경우 null 반환
-	return null;
+        // 다른 코드...
+    }
+
+    //TODO findId 메서드 추가
+    //입력받은 이름과 전화번호를 memberList에 있는 이름과 전화번호를 비교해서 
+    //해당하는 아이디를 반환하는 메서드
+    private static void findId() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("-----------------------------");
+
+        System.out.print("이름: ");
+        String name = scan.nextLine();
+
+        System.out.print("전화번호: ");
+        String phoneNum = scan.nextLine();
+
+        String memberId = findMemberId(name, phoneNum);
+
+        System.out.println("-----------------------------");
+
+        if (memberId != null) {
+            System.out.println("아이디(사원번호): " + memberId + " 입니다.");
+        } else {
+            System.out.println("아이디를 찾을 수 없습니다.");
+            System.out.println();
+            System.out.println("계속 진행하시겠습니까?(Y/N)");
+            System.out.print("입력: ");
+            
+            Main.answer = scan.nextLine();
+            
+            if(Main.answer.equals("Y") || Main.answer.equals("y")) {
+            	find();
+            	
+            }
+         
+        }
+    }
+
+    //TODO findMemberId 메서드 추가
+    //memberList에 있는 name과 phoneNum을 비교 후 해당하는 아이디 반환
+    public static String findMemberId(String name, String phoneNum) {
+        for (Members member : Data.memberList) {
+            if (member.getName().equals(name) && member.getPhoneNum().equals(phoneNum)) {
+                return member.getId();
+            }
+        }
+        return null;
+    }
+
+    //TODO findPw 메서드 추가
+    //memberList에 있는 아이디(사원번호)와 phoneNum을 비교 후 해당하는 비밀번호 반환
+    private static void findPw() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("아이디(사원번호): ");
+        String Id = scan.nextLine();
+
+        System.out.print("전화번호: ");
+        String phoneNum = scan.nextLine();
+
+        String memberPw = findMemberPw(Id, phoneNum);
+
+        if (memberPw != null) {
+            System.out.println("비밀번호: " + memberPw);
+        } else {
+            System.out.println("비밀번호를 찾을 수 없습니다.");
+        }
+    }
+
+    private static String findMemberPw(String Id, String phoneNum) {
+        for (Members member : Data.memberList) {
+            if (member.getId().equals(Id) && member.getPhoneNum().equals(phoneNum)) {
+                return member.getPw();
+            }
+        }
+        return null;
+    }
 }
-
-private static void findPw() {
-	Scanner scan = new Scanner(System.in);
-
-	System.out.print("아이디(사원번호): ");
-	String Id = scan.nextLine();
-
-	System.out.print("전화번호: ");
-	String phoneNum = scan.nextLine();
-
-	String memberPw = findMemberPw(Id, phoneNum);
-
-	if (memberPw != null) {
-		System.out.println("비밀번호: " + memberPw);
-	} else {
-		System.out.println("비밀번호를 찾을 수 없습니다.");
-	}
-}
-
-
-private static String findMemberPw(String Id, String phoneNum) {
-	for (TempMember member : Data.list) {
-		// 이름과 전화번호가 일치하는 경우 해당 멤버의 아이디를 반환
-		if (member.getMemberId().equals(Id) && member.getPhoneNum().equals(phoneNum)) {
-			return member.getPw();
-		}
-	}
-	return null;
-}
-
-
-}//find
-
--- src/com/project/factory/view/member/FindView.java
+++ src/com/project/factory/view/member/FindView.java
@@ -1,11 +1,15 @@
 package com.project.factory.view.member;
 
+import com.project.factory.member.Identify;
+
 public class FindView {
 	
+	//TODO findMenu 메서드 추가
+	//아이디(사원번호)/비밀번호 찾는 화면 
 	public static void findMenu() {
 		
 		//로그인이 된 상태이면
-		if(true) {
+		if(Identify.auth == null) {
 			System.out.println("=============================");
 			System.out.println();
 			System.out.println("1. 아이디(사원번호) 찾기");
