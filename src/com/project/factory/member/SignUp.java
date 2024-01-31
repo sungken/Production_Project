1b0d472a255dad2dd56e72090093fa6eb23b1a92
sung <tlap@naver.com>
2024-02-01 08:18:32 +0900

    회원가입 완료

--- src/com/project/factory/member/SignUp.java
+++ src/com/project/factory/member/SignUp.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 import com.project.factory.resource.Data;
 import com.project.factory.resource.Members;
 import com.project.factory.resource.Path;
+import com.project.factory.view.MainView;
 
 public class SignUp {
 
@@ -83,22 +84,17 @@ public class SignUp {
 							System.out.println("회원가입이 완료되었습니다.");
 							Members member = new Members(id, pw, name, humanNum, phoneNum, address, "2", depart, id + "@auto.com");
 							Data.memberList.add(member);
-							System.out.println("엔터를 치시면 초기화면으로 돌아갑니다.");
-							String a = scan.nextLine();
+							
+							//엔터를 치면 돌아가는 메서드
+							MainView.pause();
 							loop = false;
 						} else {
 							System.out.println("비밀번호를 조건에 맞게 다시 입력하세요.");
 						}
 					}
-											//엔터 추가 
-					
-					
-//				} else {
-//					System.out.println("사원번호를 잘못 입력하셨습니다.");
-//					System.out.println();
-//					System.out.println("계속 진행하시겠습니까?(Y/N)");
-//					System.out.print("입력: ");
-					
+				} else {
+					System.out.println("사원번호를 잘못 입력하셨습니다.");
+					MainView.checkContinue();
 				}
 			}
 
