2661090368828db30f281ee89b172c4a447b2bed
SSong-sh <tjdgp0817@gmail.com>
2024-02-05 21:18:57 +0900

    feat: 유통부 직원 배송지 확인 및 유통 기능 완성(재고 제거 필요)

---
+++ src/com/project/factory/view/member/DistributionView.java
@@ -0,0 +1,11 @@
+package com.project.factory.view.member;
+
+import com.project.factory.Today;
+
+public class DistributionView {
+
+	public static void DistributionMemberView(){
+		
+		System.out.println(Today.day());
+	}
+}
