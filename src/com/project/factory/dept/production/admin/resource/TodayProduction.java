b591869646effe90046ae9bbfcc6804649a403b3
Jang <wjdgns9904@naver.com>
2024-02-13 11:50:55 +0900

    feat: 주석 추가

--- src/com/project/factory/dept/production/admin/resource/TodayProduction.java
+++ src/com/project/factory/dept/production/admin/resource/TodayProduction.java
@@ -19,40 +19,55 @@ public class TodayProduction {
 		this.todayDefectiveNum = todayDefectiveNum;
 		this.model = model;
 	}
-
+	/**
+	 * 하루생산량 생성자이다.
+	 */
 	public TodayProduction() {
 		// TODO Auto-generated constructor stub
 	}
-
+	/**하루 생산량을 반환하는 메서드이다.
+	 * 
+	 * @return 하루 생산량
+	 */
 	public int getTodayProductNum() {
 		return todayProductNum;
 	}
-
+	/**하루 생산량을 설정하는 메서드이다.
+	 * 
+	 * @param todayProductNum 하루 생산량
+	 */
 	public void setTodayProductNum(int todayProductNum) {
 		this.todayProductNum = todayProductNum;
 	}
-
+	/**
+	 * 하루 불량품 개수를 반환하는 메서드이다.
+	 * @return	하루 불량품 개수
+	 */
 	public int getTodayDefectiveNum() {
 		return todayDefectiveNum;
 	}
-
+	/**
+	 * 하루 불량품 개수를 설정하는 메서드이다.
+	 * @param todayDefectiveNum	하루 불량품 개수
+	 */
 	public void setTodayDefectiveNum(int todayDefectiveNum) {
 		this.todayDefectiveNum = todayDefectiveNum;
 	}
-
+	/**모델 이름을 반환하는 메서드이다.
+	 * 
+	 * @return 모델이름
+	 */
 	public String getModel() {
 		return model;
 	}
-
+	/**모델 이름을 설정하는 메서드이다.
+	 * 	
+	 * @param model 모델 이름
+	 */
 	public void setModel(String model) {
 		this.model = model;
 	}
-	
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
-	}
+
 
 	
 	
